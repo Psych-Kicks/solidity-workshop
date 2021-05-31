1294cd79321b797a7d68c8b6876f4604d99dfe54
Psych-Kicks <orhan_yil@msn.com>
2021-05-31 15:45:39 +0300

    MappingExample

---
+++ SimpleMappingExample.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+// Author   : Orhan YILDIRIM
+// Email    : orhan_yil@msn.com
+// GitHub   : @Psych-Kicks
+// Medium   : @orhan_yildirim
+// Twitter  : @Psych_Kicks
+
+pragma solidity ^0.8.4;
+
+contract SimpleMappingExample {
+
+    mapping(uint => bool) public myMapping;
+    mapping(address => bool) public myAddressMapping;
+    mapping(uint => mapping(uint => bool)) uintUintBoolMapping;
+    
+    function setUintUintBoolMapping(uint _index1, uint _index2, bool _value) public {
+        uintUintBoolMapping[_index1][_index2] = _value;
+    }
+    
+    function getUintUintBoolMapping(uint _index1, uint _index2) public view returns (bool) {
+        return uintUintBoolMapping[_index1][_index2];
+    }
+    
+
+    function setValue(uint _index) public {
+        myMapping[_index] = true;
+    }
+
+    function setMyAddressToTrue() public {
+        myAddressMapping[msg.sender] = true;
+    }
+}
