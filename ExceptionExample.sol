6e33e55166e647cfc7d1e8d19e23ed02d37a5088
Psych-Kicks <orhan_yil@msn.com>
2021-06-01 14:01:04 +0300

    AssertRequire_Simplicistic_Wallet

--- ExceptionExample.sol
+++ ExceptionExample.sol
@@ -9,6 +9,10 @@ pragma solidity ^0.6.12;
 
 contract ExceptionExample {
 
+    // Use require for input validation
+    // Use assert for checking internal states
+    // Simplicistic wallet
+
     mapping(address => uint) public balanceReceived;
 
     function receiveMoney() public payable {
@@ -16,10 +20,11 @@ contract ExceptionExample {
     }
 
     function withdrawMoney(address payable _to, uint _amount) public {
-        if(_amount <= balanceReceived[msg.sender]) {
+        //if(_amount <= balanceReceived[msg.sender]) {
+            require(_amount <= balanceReceived[msg.sender], "Not enough funds, aborting");   
             balanceReceived[msg.sender] -= _amount;
             _to.transfer(_amount);  
-        }
+        //} 
     }
 }
 
