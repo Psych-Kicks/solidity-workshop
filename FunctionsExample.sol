220eda78a65b8a64778783b60812d0ef73d6fb10
Psych-Kicks <orhan_yil@msn.com>
2021-06-01 14:01:46 +0300

    MappingExample

---
+++ FunctionsExample.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+// Author   : Orhan YILDIRIM
+// Email    : orhan_yil@msn.com
+// GitHub   : @Psych-Kicks
+// Medium   : @orhan_yildirim
+// Twitter  : @Psych_Kicks
+
+pragma solidity ^0.5.13;
+
+contract FunctionsExample {
+
+    mapping(address => uint) public balanceReceived;
+
+    function receiveMoney() public payable {
+        assert(balanceReceived[msg.sender] + msg.value >= balanceReceived[msg.sender]);
+        balanceReceived[msg.sender] += msg.value;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(_amount <= balanceReceived[msg.sender], "not enough funds.");
+        assert(balanceReceived[msg.sender] >= balanceReceived[msg.sender] - _amount);
+        balanceReceived[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
+
+}
