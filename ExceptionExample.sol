fe384e6530783f0f4253af1f44d9b171450f4368
Psych-Kicks <orhan_yil@msn.com>
2021-06-01 07:10:25 +0300

    ExceptionHandlingWithAssertRequireIf

---
+++ ExceptionExample.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+// Author   : Orhan YILDIRIM
+// Email    : orhan_yil@msn.com
+// GitHub   : @Psych-Kicks
+// Medium   : @orhan_yildirim
+// Twitter  : @Psych_Kicks
+
+pragma solidity ^0.6.12;
+
+contract ExceptionExample {
+
+    mapping(address => uint) public balanceReceived;
+
+    function receiveMoney() public payable {
+        balanceReceived[msg.sender] += msg.value;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
+        if(_amount <= balanceReceived[msg.sender]) {
+            balanceReceived[msg.sender] -= _amount;
+            _to.transfer(_amount);  
+        }
+    }
+}
+
